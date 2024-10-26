da3fa9754139f450235639aa77f142c549bb6c03
Bastian <121909049+Basty-O@users.noreply.github.com>
2024-10-26 02:50:57 -0300

    Update layout.jsx

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -12,15 +12,15 @@ const Layout = ()  => {
 		<BrowserRouter>
 			<div className='layout'>
 				<h1 className='layout__title'>
-				<img src="https://upload.wikimedia.org/wikipedia/commons/4/47/Logo_UTFSM.png" style={{ width: '120px', height: '90px',  float: 'left' }} alt="Logo" />
-					<span style={{ color: '#D8BC5E', marginLeft: '20px', fontSize: '60px' }}>P</span>
-                    <span style={{ color: '#E5E5E5',marginLeft:'0PX', fontSize: '60px' }}>lataforma</span>
-					<span style={{ color: '#D8BC5E', marginLeft: '20px', fontSize: '60px' }}></span>
-                    <span style={{ color: '#E5E5E5',marginLeft:'0PX', fontSize: '60px' }}>de</span>
-					<span style={{ color: '#D8BC5E', marginLeft: '20px', fontSize: '60px' }}>A</span>
-                    <span style={{ color: '#E5E5E5',marginLeft:'0PX', fontSize: '60px' }}>yudantías</span>
-					<span style={{ color: '#D8BC5E', marginLeft: '20px', fontSize: '60px' }}>U</span>
-                    <span style={{ color: '#E5E5E5',marginLeft:'0PX', fontSize: '60px' }}>nificada</span>
+				<img src="https://upload.wikimedia.org/wikipedia/commons/4/47/Logo_UTFSM.png" style={{ width: '110px', height: '80px',  float: 'left' }} alt="Logo" />
+					<span style={{ color: '#D8BC5E', marginLeft: '20px', fontSize: '50px' }}>P</span>
+                    <span style={{ color: '#E5E5E5',marginLeft:'0PX', fontSize: '50px' }}>lataforma</span>
+					<span style={{ color: '#D8BC5E', marginLeft: '20px', fontSize: '50px' }}></span>
+                    <span style={{ color: '#E5E5E5',marginLeft:'0PX', fontSize: '50px' }}>de</span>
+					<span style={{ color: '#D8BC5E', marginLeft: '20px', fontSize: '50px' }}>A</span>
+                    <span style={{ color: '#E5E5E5',marginLeft:'0PX', fontSize: '50px' }}>yudantías</span>
+					<span style={{ color: '#D8BC5E', marginLeft: '20px', fontSize: '50px' }}>U</span>
+                    <span style={{ color: '#E5E5E5',marginLeft:'0PX', fontSize: '50px' }}>nificada</span>
 				</h1>
 				<NavBar />
 				<div className='layout__page'>
@@ -36,3 +36,4 @@ const Layout = ()  => {
 }
 
 export default Layout
+
