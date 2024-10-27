131e13a902e7f072f7b18b436cec5bea829dfb38
Bastian <121909049+Basty-O@users.noreply.github.com>
2024-10-27 04:11:26 -0300

    Update table.jsx

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -3,48 +3,34 @@ import SeleccionarPostulante from './button';
 
 function Tabla() {
 	return (
-		<table className='table'>
+function Tabla() {
+	return (
+            <table className="table table-striped table-bordered table">
 			<thead className='table__head'>
 				<tr>
-					<th>Nombre</th>
-					<th>ROL</th>
-					<th>Nota de Aprobacion</th>
-					<th>Semestre Cursado</th>
-					<th>Catedra</th>
-					<th>Laboratorio</th>
-					<th>Corrector</th>
-					<th></th>
+				<th style={{ backgroundColor: '#002244', color: 'white' }}>Sigla</th>
+				<th style={{ backgroundColor: '#002244', color: 'white' }}>Ramo</th>
+				<th style={{ backgroundColor: '#002244', color: 'white' }}>Postulante</th>
+				<th style={{ backgroundColor: '#002244', color: 'white' }}></th>
 				</tr>
 			</thead>
 			<tbody className='table__body'>
 				<tr>
-					<td>Alessandro Cintolesi Rodriguez</td>
-					<td>202173541-0</td>
-					<td>81</td>
-					<td>2024-1</td>
-					<td>Postula</td>
-					<td>No Postula</td>
-					<td>Postula</td>
+					<td>INF-295</td>
+					<td>Inteligencia Artificial</td>
+					<td>4</td>
 					<th><SeleccionarPostulante /></th>
 				</tr>
 				<tr>
-					<td>Ignacio Muñoz Sanchez</td>
-					<td>202173620-4</td>
-					<td>86</td>
-					<td>2024-1</td>
-					<td>Postula</td>
-					<td>No Postula</td>
-					<td>Postula</td>
+					<td>INF-343</td>
+					<td>Sistemas Distribuidos</td>
+					<td>6</td>
 					<th><SeleccionarPostulante /></th>
 				</tr>
 				<tr>
-					<td>Bastian Ortega Antillana</td>
-					<td>202173547-K</td>
-					<td>84</td>
-					<td>2024-1</td>
-					<td>Postula</td>
-					<td>No Postula</td>
-					<td>Postula</td>
+					<td>INF-285</td>
+					<td>Computación Cientifica</td>
+					<td>5</td>
 					<th><SeleccionarPostulante /></th>
 				</tr>
 			</tbody>
